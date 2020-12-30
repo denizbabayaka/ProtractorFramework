6be3fa3e392f3e100b6eaac8e581c6541ea9e8b6
denizbabayaka <56898154+denizbabayaka@users.noreply.github.com>
2020-12-30 12:51:02 -0500

    my first project commit

---
+++ tests/calculator.js
@@ -0,0 +1,85 @@
+let homepage=require('../pages/homepage');
+
+
+
+// describe is a test suit and inside there is an another function
+describe('demo calculator test', function () {
+
+    it('addition test', function () {
+
+        //  browser.get('https://juliemr.github.io/protractor-demo/');
+
+        // this method comes from homepage 
+        homepage.get('https://juliemr.github.io/protractor-demo/');
+        
+        
+
+        //var input = element(by.model('first'));
+        //input.sendKeys('5');
+
+        homepage.enterFirstNumber('5');
+
+        //var input = element(by.model('second'));
+        //input.sendKeys('2');
+
+        homepage.enterSecondNumber('2');
+
+        //element(by.css('[ng-click="doAddition()"]')).click();
+
+        homepage.clickGo();
+
+        //var result = element(by.cssContainingText('.ng-binding', '7'));
+
+        //expect(result.getText()).toEqual('7');
+
+        homepage.verifyResult('7');
+
+
+
+
+
+        browser.sleep(2000);
+    });
+
+    it('substraction test', function () {
+
+        //  browser.get('https://juliemr.github.io/protractor-demo/');
+
+        // this method comes from homepage 
+        homepage.get('https://juliemr.github.io/protractor-demo/');
+        
+        
+
+        //var input = element(by.model('first'));
+        //input.sendKeys('5');
+
+        homepage.enterFirstNumber('5');
+
+        //var input = element(by.model('second'));
+        //input.sendKeys('2');
+
+        homepage.enterSecondNumber('2');
+
+        //element(by.css('[ng-click="doAddition()"]')).click();
+
+        homepage.clickGo2();
+
+        //var result = element(by.cssContainingText('.ng-binding', '7'));
+
+        //expect(result.getText()).toEqual('7');
+
+        homepage.verifyResult('7');
+
+
+
+
+
+        browser.sleep(2000);
+    });
+
+    
+
+
+    
+
+});
